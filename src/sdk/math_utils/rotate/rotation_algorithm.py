51a3c2efd64976fd831e73f3863c4f2d974b6c3a
GabrielFernandez1 <gabriel.fernandez1@udc.es>
2025-05-29 11:24:38 +0200

    [It4] Implementación RotationAlgorithm

--- src/sdk/math_utils/rotate/rotation_algorithm.py
+++ src/sdk/math_utils/rotate/rotation_algorithm.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import numpy as np
 from sdk.cuda_manager.abstract_cuda_manager import CudaManager
 from sdk.cuda_manager.implementations.auto_cuda_manager import AutoCudaManager
 from sdk.math_utils.rotate.abstract_rotation_algorithm import (
@@ -20,5 +21,22 @@ class RotationAlgorithm(AbstractRotationAlgorithm):
             )
         )
 
-    def transform_f32():
-        pass
+    def transform_f32(
+        self,
+        in_matrix: np.ndarray, 
+        t_matrix: np.ndarray,
+        n: int
+    ) -> np.array[np.double]:
+
+        transform_f32_out: list = self.cuda_manager.run_program(
+            "transform_f32",
+            [1],
+            {1: [(len(in_matrix),), np.double]},
+            (1, 1, 1),
+            (int(n), 1, 1),
+            in_matrix, 
+            np.empty_like(in_matrix),
+            t_matrix, 
+            n
+        )
+        return transform_f32_out[0]
